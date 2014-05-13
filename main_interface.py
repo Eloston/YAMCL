4b8e6feccd157ad270253de4a5e1d2c869a79c0a
Eloston <Eloston@users.noreply.github.com>
2014-05-12 17:26:29 -0700

    Added argument parsing

--- main_interface.py
+++ main_interface.py
@@ -14,6 +14,7 @@ along with YAMCL.  If not, see {http://www.gnu.org/licenses/}.
 '''
 
 import sys
+import argparse
 from PySide import QtCore, QtGui
 
 import yamcl.main
@@ -22,15 +23,19 @@ import graphical_interface.MainGUI
 import graphical_interface.SplashScreen
 
 if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("--data-path", help="Override path to YAMCL's data directory", type=str, default=str())
+    arg_parser.add_argument("--java-path", help="Override the Java executable to use", type=str, default=str())
+    arg_returns = arg_parser.parse_args()
+
     app = QtGui.QApplication(sys.argv)
     splash_screen = graphical_interface.SplashScreen.SplashScreen()
     splash_screen.show()
 
     main_launcher = yamcl.main.Launcher()
-    yamcl_data_path = str()
     startup_status = None
     while not startup_status == "SUCCESS":
-        startup_status = main_launcher.startup(yamcl_data_path)
+        startup_status = main_launcher.startup(data_path=arg_returns.data_path, java_command=arg_returns.java_path)
         if startup_status == "FAIL_DATACORRUPT":
             clicked_button = QtGui.QMessageBox.critical(splash_screen, "YAMCL Data Error", "Your YAMCL data at " + str(main_launcher.ROOT_PATH) + " is missing or corrupt.\nWould you like to initialize the directory? Click Cancel to exit YAMCL.", QtGui.QMessageBox.Yes | QtGui.QMessageBox.Cancel)
             if clicked_button == QtGui.QMessageBox.Yes:
